8ed1f4acc4b3a7c38bc62efb8aeb210ca874b09a
Vira <viraproject1308@gmail.com>
2026-01-20 21:48:58 +0200

    fix

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api = Api(os.environ.get("AIRTABLE_API_KEY"))
-table = api.table(os.environ.get("BASE_ID"), 'tblqGPYW8tuusMMrl') # base_id, table_id
+# CONFIG
+AIRTABLE_API_KEY = os.environ.get("AIRTABLE_API_KEY")
+BASE_ID = os.environ.get("BASE_ID")
+REQUESTS_TABLE_ID = os.environ.get("REQUESTS_TABLE_ID")
+CONSULTANTS_TABLE_ID = os.environ.get("CONSULTANTS_TABLE_ID")
 
-print(table.all())
+REPORT_FILE = "weekly_report.csv"
+
+# INIT
+api = Api(AIRTABLE_API_KEY)
+requests_table = api.table(BASE_ID, REQUESTS_TABLE_ID)
+consultants_table = api.table(BASE_ID, CONSULTANTS_TABLE_ID)
+
